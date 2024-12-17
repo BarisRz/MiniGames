6f673fb3bd252059df6c3203c61f61891cee2e89
Baris G <barisg@pm.me>
2024-12-17 16:30:57 +0100

    Update styles and text in ErrorPage component

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -5,13 +5,13 @@ export default function ErrorPage() {
   return (
     <div className="bg-primary dark:bg-dark-primary flex justify-center items-center h-screen">
       <div className="flex flex-col items-center">
-        <Ban size={128} className="text-title mb-2" />
-        <h1 className="text-3xl text-title dark:text-black max-[500px]:text-xl">
+        <Ban size={128} className="text-title mb-2 dark:text-dark-title" />
+        <h1 className="text-3xl text-title dark:text-dark-title max-[500px]:text-xl ">
           404 : Page introuvable
         </h1>
         <Link
           to={"/"}
-          className="bg-secondary mt-4 p-4 rounded-lg border border-secondary hover:border-accent"
+          className="bg-secondary dark:bg-dark-secondary hover:bg-secondary-accent text-title dark:hover:bg-dark-secondary2 dark:border-dark-secondary dark:hover:border-accent dark:text-dark-title mt-4 p-4 rounded-lg border border-secondary hover:border-accent transition-colors"
         >
           Retour à l'accueil
         </Link>
